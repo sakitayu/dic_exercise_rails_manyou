0c244cd48fdbbaadb01fb86e90ceb51c07c3e8fb
sakitayu <sakitakitakita@gmail.com>
2019-11-14 19:05:28 +0900

    タイムゾーンを日本（東京）に設定

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module ManyouTaskApp
                        controller_specs: false,
                        request_specs: false
     end
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
 

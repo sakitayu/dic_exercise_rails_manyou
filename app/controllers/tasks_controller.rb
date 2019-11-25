2bf6d1785feb4091eae0d9720dcf5cd6a2076d50
sakitayu <sakitakitakita@gmail.com>
2019-11-25 16:04:17 +0900

    ログインしていないのにタスクのページに飛ぼうとした場合、ログインページに遷移する機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,25 +3,29 @@ class TasksController < ApplicationController
   PER = 10
 
   def index
-    @tasks = Task.where(user_id: current_user.id)
-    if params[:name].blank? && params[:status].blank?
-      @tasks = @tasks.page(params[:page]).per(PER)
-      if params[:sort_expired].blank? && params[:sort_priority].blank?
-        @tasks = @tasks.order(created_at: :desc)
-      elsif params[:sort_expired].blank?
-        @tasks = @tasks.order(priority: :asc)
+    if logged_in?
+      @tasks = Task.where(user_id: current_user.id)
+      if params[:name].blank? && params[:status].blank?
+        @tasks = @tasks.page(params[:page]).per(PER)
+        if params[:sort_expired].blank? && params[:sort_priority].blank?
+          @tasks = @tasks.order(created_at: :desc)
+        elsif params[:sort_expired].blank?
+          @tasks = @tasks.order(priority: :asc)
+        else
+          @tasks = @tasks.order(expired_at: :desc)
+        end
+      elsif params[:name].blank?
+        @tasks = Task.page(params[:page]).per(PER).status_search(params[:status])
+        flash[:notice] = "「#{params[:status]}」の検索結果"
+      elsif params[:status].blank?
+        @tasks = Task.page(params[:page]).per(PER).name_search(params[:name])
+        flash[:notice] = "「#{params[:name].slice(0, 6)}」の検索結果"
       else
-        @tasks = @tasks.order(expired_at: :desc)
+        @tasks = Task.page(params[:page]).per(PER).name_and_status_search(params[:name], params[:status])
+        flash[:notice] = "「#{params[:status]}」と「#{params[:name].slice(0, 6)}」の検索結果"
       end
-    elsif params[:name].blank?
-      @tasks = Task.page(params[:page]).per(PER).status_search(params[:status])
-      flash[:notice] = "「#{params[:status]}」の検索結果"
-    elsif params[:status].blank?
-      @tasks = Task.page(params[:page]).per(PER).name_search(params[:name])
-      flash[:notice] = "「#{params[:name].slice(0, 6)}」の検索結果"
     else
-      @tasks = Task.page(params[:page]).per(PER).name_and_status_search(params[:name], params[:status])
-      flash[:notice] = "「#{params[:status]}」と「#{params[:name].slice(0, 6)}」の検索結果"
+      redirect_to sessions_new_path
     end
     
   end

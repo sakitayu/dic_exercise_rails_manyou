13fd3222a0e1013027ed63bf1a48ebc9936c00fb
sakitayu <sakitakitakita@gmail.com>
2019-11-27 10:48:03 +0900

    管理者画面からのユーザー追加機能とユーザー削除機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,28 +1,38 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
   def new
-    if logged_in?
-      redirect_to user_path(current_user.id)
-    else
+    #if logged_in?
+      #redirect_to user_path(current_user.id)
+    #else
       @user = User.new
-    end
+    #end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to tasks_path
+      redirect_to admin_users_path
     else
       render 'new'
     end
   end
 
   def show
-    if params[:id] == "#{current_user.id}"
+    #if params[:id] == "#{current_user.id}"
       @user = User.find(params[:id])
-    else
-      redirect_to user_path(current_user.id)
-    end
+    #else
+      #redirect_to user_path(current_user.id)
+    #end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザーを削除しました！"
   end
 
   private

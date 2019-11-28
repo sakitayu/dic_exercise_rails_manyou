96e8cf76d6ee63457a32c2410224877fbb714d5a
sakitayu <sakitakitakita@gmail.com>
2019-11-28 13:13:49 +0900

    validationでユーザのアドレスにユニーク制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_update :ensure_admin
 
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password

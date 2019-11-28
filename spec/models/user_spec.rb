3176bcda7e93cb03ec302a3da4de374b1e641c0b
sakitayu <sakitakitakita@gmail.com>
2019-11-28 16:40:07 +0900

    ユーザのmodelテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,27 +3,38 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "nameが空ならバリデーションが通らない" do
-    task = User.new(name: '', detail: '失敗テスト')
-    expect(task).not_to be_valid
+    user = User.new(name: '', email: 'test@example.com', password: 'password')
+    expect(user).not_to be_valid
   end
 
-  it "detailが空ならバリデーションが通らない" do
-    task = User.new(name: '失敗テスト', detail: '')
-    expect(task).not_to be_valid
+  it "nameが31文字以上ならバリデーションが通らない" do
+    user = User.new(name: '12345678910-12345678910-12345678910-', email: 'test@example.com', password: 'password')
+    expect(user).not_to be_valid
   end
 
-  it "nameとdetailに内容が記載されていればバリデーションが通る" do
-    task = User.new(name: '成功テスト', detail: '成功テスト')
-    expect(task).to be_valid
+  it "emailが空ならバリデーションが通らない" do
+    user = User.new(name: 'test', email: '', password: 'password')
+    expect(user).not_to be_valid
   end
 
-  it "検索文字列に一致したタスクが一覧に表示される" do
-    task1 = User.create(name: 'タスク1', detail: 'タスク1の中身', expired_at: '2019-11-22 12:27:00', status: '完了')
-    task2 = User.create(name: 'タスク2', detail: 'タスク2のなかみ', expired_at: '2019-11-23 12:27:00', status: '未着手')
-    task3 = User.create(name: 'タスク3', detail: 'タスク3のナカミ', expired_at: '2019-11-24 12:27:00', status: '着手中')
-    expect(User.name_search("タスク")).to include(task1, task2, task3)
-    expect(User.status_search("未着手")).to include(task2)
-    expect(User.name_and_status_search("タスク", "着手中")).to include(task3)
-    expect(User.name_search("ﾆｬﾎﾆｬﾎﾀﾏｸﾛｰ")).to be_empty
+  it "emailが asd@asd.com のような文字列でない場合バリデーションが通らない" do
+    user = User.new(name: 'test', email: 'abcdefg', password: 'password')
+    expect(user).not_to be_valid
+  end
+
+  it "emailが255文字以上ならバリデーションが通らない" do
+    user = User.new(name: 'test', email: '123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910', password: 'password')
+    expect(user).not_to be_valid
+  end
+
+  it "emailが同じ場合バリデーションが通らない" do
+    user1 = User.create(name: 'test1', email: 'test@example.com', password: 'password')
+    user2 = User.new(name: 'test2', email: 'test@example.com', password: 'password')
+    expect(user2).not_to be_valid
+  end
+
+  it "nameとemailとpasswordに内容が条件通り(文字数、ユニーク制約、指定文字列など)記載されていればバリデーションが通る" do
+    user = User.new(name: 'test', email: 'test@example.com', password: 'password')
+    expect(user).to be_valid
   end
 end

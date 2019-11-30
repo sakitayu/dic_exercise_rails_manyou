9bd08e100fe4baea3575539772e633c8d2095530
sakitayu <sakitakitakita@gmail.com>
2019-11-30 17:26:20 +0900

    ラベリング機能を実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,4 +10,6 @@ class Task < ApplicationRecord
   scope :name_and_status_search, -> (params_name, params_status) { where("status LIKE ?", "%#{params_status}%").where("name LIKE ?", "%#{params_name}%") }
   enum priority:{高:1,中:2,低:3}
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 end

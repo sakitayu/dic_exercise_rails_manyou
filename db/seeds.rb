763c76299c7e1e55d195df3c15ae5850bf767971
sakitayu <sakitakitakita@gmail.com>
2019-12-01 14:58:06 +0900

    seeds.rbのUserに管理者シードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-1.times do |n|
+User.create!(
+  name: 'admin_master',
+  email: 'admin@example.com',
+  password: 'adminadmin',
+  admin: true,
+)
+
+10.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
@@ -14,6 +21,5 @@
                email: email,
                password: password,
                password_confirmation: password,
-               admin: admin,
                )
 end

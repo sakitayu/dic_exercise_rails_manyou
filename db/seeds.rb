881ad3d6117f3cce7d0c4bc13ca68c34f8310057
sakitayu <sakitakitakita@gmail.com>
2019-12-01 15:07:01 +0900

    seeds.rbを管理者ユーザのみ生成するように変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ User.create!(
   admin: true,
 )
 
-10.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
+# 10.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password,
+#                )
+# end
